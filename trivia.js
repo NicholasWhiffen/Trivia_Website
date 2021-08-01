e7cbe709c7ceba325b38e6841c39d0f64ee9aabe
Nicholas <nicholaswhiffen@hotmail.com>
2021-07-31 22:54:14 -0230

    "-redesigned program from buttons to textbox input -added basic funtions to js"

--- trivia.js
+++ trivia.js
@@ -1,26 +1,110 @@
 "use strict";
 
 const $ = selector => document.querySelector(selector);
+let questions = [];
+const correct = false;
+let score = 0;
+const answer = $("#answer");
+const a = $("#a");
+const b = $("#b");
+const c = $("#c");
+const d = $("#d");
+const error = $("#error");
+console.log(score);
+                    
+const startGame = () => {
+    questions = [["What colour is Mace Windu's lightsaber?",  
+                    "Purple",
+                    "Yellow",
+                    "Blue",
+                    "Red",
+                    "Purple"],
+                    ["What movie does Yoda first appear?",
+                    "Return of the Jedi",
+                    "The Phantom Menace",
+                    "The Empire Strikes Back",
+                    "Revenge of the Sith",
+                    "The Empire Strikes Back"],
+                    ["C-3P0 is fluent in how many languages?",
+                    "10 thousand",
+                    "1 billion",
+                    "60 million",
+                    "20 million",
+                    "60 million"],
+                    ["What movie does Yoda first appear?",
+                    "Return of the Jedi",
+                    "The Phantom Menace",
+                    "The Empire Strikes Back",
+                    "Revenge of the Sith"],
+                    ["What movie does Yoda first appear?",
+                    "Return of the Jedi",
+                    "The Phantom Menace",
+                    "The Empire Strikes Back",
+                    "Revenge of the Sith"],
+                    ["What movie does Yoda first appear?",
+                    "Return of the Jedi",
+                    "The Phantom Menace",
+                    "The Empire Strikes Back",
+                    "Revenge of the Sith"],
+                    ["What movie does Yoda first appear?",
+                    "Return of the Jedi",
+                    "The Phantom Menace",
+                    "The Empire Strikes Back",
+                    "Revenge of the Sith"],
+                    ["What movie does Yoda first appear?",
+                    "Return of the Jedi",
+                    "The Phantom Menace",
+                    "The Empire Strikes Back",
+                    "Revenge of the Sith"],
+                    ["What movie does Yoda first appear?",
+                    "Return of the Jedi",
+                    "The Phantom Menace",
+                    "The Empire Strikes Back",
+                    "Revenge of the Sith"],
+                    ["What movie does Yoda first appear?",
+                    "Return of the Jedi",
+                    "The Phantom Menace",
+                    "The Empire Strikes Back",
+                    "Revenge of the Sith"]];
+    $("#questionsContainer").classList.remove("hide");
+    $("#start").classList.toggle("hide");
+    nextQuestion()
+};
 
-const questions = [["What is the name of Mario's hat in Super Mario Odyssey?\nA. Hatty\nB. Cappy\n C. Charlie\n D. Dr. Hat", "B"]]
+const nextQuestion = () => {
+    answer.value = "";
+    $("#question").textContent = questions[0][0];
+    a.textContent = (questions[0][1]);
+    b.textContent = (questions[0][2]);
+    c.textContent = (questions[0][3]);
+    d.textContent = (questions[0][4]);
+};
 
-const questionsContainer = document.getElementById("questionsContainer");
-const startButton = document.getElementById("start");
+const checkAnswer = () => {
+    if((answer.value).toLowerCase() == ""){
+        error.textContent = "No answer was input";
+    } else if((answer.value).toLowerCase() == questions[0][5].toLowerCase()){
+        error.textContent = "";
+        score += 1;
+        console.log(score);
+        questions.splice(0,1);
+        nextQuestion();
+    } else{
+        answer.value = "";
+        error.textContent = "Not a valid answer";
+    }
+};
 
-const askQuestion = () => {
-
+const endGame = () => {
+    $("#questionsContainer").classList.toggle("hide");
     
-    questionsContainer.classList.remove("hide");
-    startButton.classList.toggle("hide");
-
-    let question = questions[0][0];
-    const askQuestion = document.createTextNode(question);
-    const questionP = $("#question").querySelector("p");
-    questionP.innerText = askQuestion;
-
 }
 
 document.addEventListener("DOMContentLoaded", () => {
 	// add event handlers
-	$("#start").addEventListener("click", askQuestion);
+	$("#start").addEventListener("click", startGame);
+    $("#submit").addEventListener("click", checkAnswer);
+
+
+    
 });
